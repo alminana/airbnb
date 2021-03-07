640942caef488df1f5307330085f25f295333107
alminana <alminanaeilerbutad@yahoo.com>
2021-03-08 03:05:27 +0800

    Updates

---
+++ precache-manifest.2a4f580469b0e4c3db9849f073c6752d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6ef99605bab4fd73d4a9f8060cbe1d",
+    "url": "/airbnb/index.html"
+  },
+  {
+    "revision": "e46ac5e051d64a41853f",
+    "url": "/airbnb/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "ec818205278cfad9e48f",
+    "url": "/airbnb/static/css/main.98ea50fc.chunk.css"
+  },
+  {
+    "revision": "e46ac5e051d64a41853f",
+    "url": "/airbnb/static/js/2.38345d37.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/airbnb/static/js/2.38345d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec818205278cfad9e48f",
+    "url": "/airbnb/static/js/main.172d0318.chunk.js"
+  },
+  {
+    "revision": "fd9c6bfcd53d097ec853",
+    "url": "/airbnb/static/js/runtime-main.10d687b2.js"
+  }
+]);
